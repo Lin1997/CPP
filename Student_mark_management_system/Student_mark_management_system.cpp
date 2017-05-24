b5651a91745cd2786300fe08c523dde4ce4bd890
jingchun Lin <jingchun.Lin@studentpartner.com>
2017-05-24 11:29:51 +0800

    Day2.1

--- Student_mark_management_system/Student_mark_management_system.cpp
+++ Student_mark_management_system/Student_mark_management_system.cpp
@@ -5,6 +5,8 @@
 #include <iostream>
 using namespace std;
 
+enum logintype { Nologin, Stu, Tea, Adm };				//登陆状态
+logintype state = Nologin;
 class Name
 {
 private:
@@ -380,36 +382,51 @@ public:
 };
 TeaNode * TeaList::pHead = NULL;				//初始化
 
-
-enum menutype { first, second, third };
-void menu(menutype type);					//显示菜单函数
-bool login();					//登陆函数
+void menu(logintype state);					//显示菜单函数
+logintype login();					//登陆函数
 void encrypt();				//加密函数
 void decrypt();				//解密函数
 
-void menu(menutype type)
+void menu(logintype type)
 {
+	system("cls");
+	for (int i = 0; i < 48; i++)				//标题
+	{
+		cout << ' ';
+	}
+	cout << "学生成绩管理系统" << endl;
+
 	switch (type)
 	{
-	case first:
-		for (int i = 0; i < 48; i++)				//标题
-		{
-			cout << ' ';
-		}
-		cout << "学生成绩管理系统" << endl;
+	case Nologin:
+		cout << "未登录" << endl;
 		break;
-	case second:
-
+	case Stu:
+		cout << "功能菜单：" << endl;
+		cout << "a.查询自己的成绩\t\t"<< "b.查询全班成绩\t\t" << endl
+			<< "c.导出成绩\t\t\t"<< "d.修改密码\t\t" << endl
+			<< "q.退出系统" << endl;
 		break;
-	case third:
-
+	case Tea:
+		cout << "功能菜单：" << endl;
+		cout << "a.录入(导入)学生成绩\t"<< "b.查询全班成绩\t\t" << endl
+			<< "c.成绩分析\t\t\t"<< "d.添加学生\t\t" << endl
+			<< "e.删除学生\t\t\t"<< "f.导出全班成绩\t\t" << endl
+			<< "g.修改密码\t\t" << endl
+			<< "q.退出系统" << endl;
+		break;
+	case Adm:
+		cout << "功能菜单：" << endl;
+		cout << "a.添加学生\t\t" << "b.删除学生\t\t" << endl
+			<< "c.添加教师\t\t" << "d.删除教师\t\t" << endl
+			<< "q.退出系统" << endl;
 		break;
 	default:
 		cout << "type参数错误！" << endl;
 	}
 }
 
-bool login()
+logintype login()
 {
 	char name[30];
 	char password[30];
@@ -417,63 +434,110 @@ bool login()
 	{
 		cout << "老师列表和学生列表都为空，请以管理员账户登陆。" << endl;
 	}
-	cout << "请输入账户名：" << endl;
-	cin.getline(name, 30);
-	StuNode *pStu = StuList::SearchName(name);
-	TeaNode *pTea = TeaList::SearchName(name);
-	if (pStu)
+	for (int i = 2; i >= 0; i--)
 	{
+		cout << "请输入账户名：" << endl;
+		cin.getline(name, 30);
 		cout << "请输入密码：" << endl;
 		cin.getline(password, 30);
-		if (pStu->stu.verify(password))
+		StuNode *pStu = StuList::SearchName(name);
+		TeaNode *pTea = TeaList::SearchName(name);
+		if (pStu)
 		{
-			cout << "登陆成功：" << endl;
-			cout << "当前账户：" << pStu->stu.getName() <<"(学生)"<< endl;
-			return true;
+			if (pStu->stu.verify(password))
+			{
+				cout << "登陆成功：" << endl;
+				cout << "当前账户：" << pStu->stu.getName() << "(学生)" << endl;
+				return Stu;
+			}
+			else
+			{
+				cout << "账户或密码错误！";
+				cout << "剩余" << i << "次机会" << endl;
+			}
+		}
+		else if (pTea)
+		{
+			if (pTea->tea.verify(password))
+			{
+				cout << "登陆成功：" << endl;
+				cout << "当前账户：" << pTea->tea.getName() << "(教师)" << endl;
+				return Tea;
+			}
+			else
+			{
+				cout << "账户或密码错误！";
+				cout << "剩余" << i << "次机会" << endl;
+			}
 		}
 		else
 		{
-			cout << "账户或密码错误，登陆失败！" << endl;
-			return false;
+			if (strcmp(admin.getName(), name) == 0 && admin.verify(password))
+			{
+				cout << "登陆成功：" << endl;
+				cout << "当前账户：" << admin.getName() << "(管理员)" << endl;
+				return Adm;
+			}
+			else
+			{
+				cout << "账户或密码错误！";
+				cout << "剩余" << i << "次机会" << endl;
+			}
 		}
 	}
-	else if (pTea)
-	{
-		cout << "请输入密码：" << endl;
-		cin.getline(password, 30);
-		if (pTea->tea.verify(password))
-		{
-			cout << "登陆成功：" << endl;
-			cout << "当前账户：" << pTea->tea.getName() <<"(教师)"<< endl;
-			return true;
-		}
-		else
-		{
-			cout << "账户或密码错误，登陆失败！" << endl;
-			return false;
-		}
-	}
-	else
-	{
-		cin.getline(password, 30);
-		if (strcmp(admin.getName(), name)==0 && admin.verify(password))
-		{
-			cout << "登陆成功：" << endl;
-			cout << "当前账户：" << admin.getName() <<"(管理员)"<< endl;
-			return true;
-		}
-		else
-		{
-			cout << "账户或密码错误，登陆失败！" << endl;
-			return false;
-		}
-	}
+	cout << "错误次数过多，登录失败！" << endl;
+	return Nologin;
 }
 
 int main()
 {
-	menu(first);
-	cout << login() << endl;
+	TeaList::add();
+	menu(Nologin);
+	state = login();
 	system("pause");
+	if (state == Nologin)
+	{
+		return 0;
+	}
+	char choice;
+	do
+	{
+		menu(state);
+		cout << "请选择操作：" << endl;
+		cin >> choice;
+		if (state == Stu)
+		{
+			switch (choice)
+			{
+			case'a':cout << "导入成绩" << endl;
+				system("pause");
+				break;
+			case'b':cout << "查询全班成绩" << endl;
+				system("pause");
+				break;
+			case'c':cout << "成绩分析" << endl;
+				system("pause");
+				break;
+			case'd':cout << "添加学生" << endl;
+				system("pause");
+				break;
+			case'e':cout << "删除学生" << endl;
+				system("pause");
+				break;
+			case'f':cout << "导出全班成绩" << endl;
+				system("pause");
+				break;
+			case'g':cout << "修改密码" << endl;
+				system("pause");
+				break;
+			case'q':cout << "退出系统" << endl;
+				system("pause");
+				break;
+			default:
+				cout << "非法输入！" << endl;
+				system("pause");
+			}
+		}
+	} while (choice != 'q');
 	return 0;
 }
